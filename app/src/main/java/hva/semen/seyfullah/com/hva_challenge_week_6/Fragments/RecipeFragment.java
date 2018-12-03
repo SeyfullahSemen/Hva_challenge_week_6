6f403516b9607352c6e25a6e090a368dbd9f7fef
seyf <seyfo12@hotmail.com>
2018-12-03 20:22:25 +0100

    Initial commit

---
+++ app/src/main/java/hva/semen/seyfullah/com/hva_challenge_week_6/Fragments/RecipeFragment.java
@@ -0,0 +1,87 @@
+package hva.semen.seyfullah.com.hva_challenge_week_6.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import hva.semen.seyfullah.com.hva_challenge_week_6.R;
+import hva.semen.seyfullah.com.hva_challenge_week_6.Retrofit.RecipeJson;
+
+/*
+ * Created by Seyfullah Semen on 25-11-2018.
+ */
+public class RecipeFragment extends Fragment {
+
+    private static final String TAG = RecipeFragment.class.getSimpleName();
+    private Unbinder unbinder;
+
+    private static final String ARG_RECIPE_ID = "arg_recipe_id";
+    private RecipeJson mData;
+
+    @BindView(R.id.imageView)
+    ImageView mImageView;
+    @BindView(R.id.text_view_recipe_name)
+    TextView mTextViewRecipeName;
+    @BindView(R.id.text_view_recipe_description)
+    TextView mTextViewRecipeDescription;
+
+
+    public static RecipeFragment newInstance(RecipeJson recipeJson) {
+        RecipeFragment fragment = new RecipeFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_RECIPE_ID, recipeJson);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mData = getArguments().getParcelable(ARG_RECIPE_ID);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View recipeView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, recipeView);
+        unbinder = ButterKnife.bind(this, recipeView);
+        mTextViewRecipeName.setText(mData.getTitle());
+        mTextViewRecipeDescription.setText(mData.getRecipeId());
+        Glide.with(this)
+                .load(mData.getImageUrl())
+                .into(mImageView);
+
+
+        return recipeView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private String loopThroughIngredients(List<String> ingredients) {
+        StringBuilder ingredient = new StringBuilder();
+        for (int i = 0; i < ingredients.size(); i++) {
+            ingredient.append("\t-\t").append(ingredients.get(i)).append("\n");
+        }
+        return ingredient.toString();
+    }
+
+
+}
---
+++ app/src/main/java/hva/semen/seyfullah/com/hva_challenge_week_6/RecipeRespond.java
@@ -0,0 +1,11 @@
+package hva.semen.seyfullah.com.hva_challenge_week_6;
+
+import hva.semen.seyfullah.com.hva_challenge_week_6.Retrofit.RecipeJson;
+
+/*
+ * Created by Seyfullah Semen on 3-12-2018.
+ */
+public class RecipeRespond {
+    int count;
+    RecipeJson[] recipes;
+}
